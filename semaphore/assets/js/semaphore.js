82e17a1dac06942b1eeb22b90df5dc7b308ebaa4
crishard <crislantorres9@gmail.com>
2021-12-01 21:50:21 -0300

    new project in production

---
+++ semaphore/assets/js/semaphore.js
@@ -0,0 +1,75 @@
+// const img = document.getElementById( 'img' );
+// const buttons = document.getElementById( 'buttons' );
+// let colorIndex = 0;
+// let intervalId = null;
+
+// const trafficLight = ( event ) => {
+//     stopAutomatic();
+//     turnOn[event.target.id]();
+// }
+
+// const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+// const changeColor = () => {
+//     const colors = ['red','yellow','green']
+//     const color = colors[ colorIndex ];
+//     turnOn[color]();
+//     nextIndex();
+// }
+
+// const stopAutomatic = () => {
+//     clearInterval ( intervalId );
+// }
+
+// const turnOn = {
+//     'red':      () => img.src = './assets/img/red.jpg',
+//     'yellow':   () => img.src = './assets/img/yellow.jpg',
+//     'green':    () => img.src = './assets/img/green.jpg',
+//     'automatic': () => intervalId = setInterval( changeColor, 1000 )
+// }
+
+// buttons.addEventListener('click', trafficLight );
+
+
+
+
+
+
+
+
+
+
+// const semaRed = document.getElementById('red');
+// const semaYellow = document.getElementById('yellow');
+// const semaGreen = document.getElementById('green');
+
+// const bottons = document.getElementById('bottons')
+// const semaphore = document.getElementById('semaphore');
+
+
+// const sema = documen.getElementById('semaphore');
+
+// function trafficLgth (event){
+//     if(event.target.id ==  'red')
+// } 
+
+
+// bottons.addEventListener('click', trafficLgth)
+
+// function semared(){
+
+//     sema.scr = "./assets/img/red.jpg"
+
+// }
+
+// function semagreen(){
+
+//     sema.scr = "./assets/img/green.jpg"
+
+// }
+
+// function semayellow(){
+
+//     sema.scr = "./assets/img/yellow.jpg"
+
+// }
