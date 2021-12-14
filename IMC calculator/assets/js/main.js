66488f9cfe8564ecda152f7294ab9e1158d882e1
crishard <crislantorres9@gmail.com>
2021-12-14 09:26:55 -0300

    complete

--- IMC calculator/assets/js/main.js
+++ IMC calculator/assets/js/main.js
@@ -2,12 +2,66 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
+    
+    const inputPeso = event.target.querySelector('#peso');
+    const inputAltura = event.target.querySelector('#altura');
 
-    console.log('Enviado');
-    setResultado('Hi guys');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso){
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if(!altura){
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+
+    const nivelImc = getNivel(imc)
+
+    const msg = `Seu IMC é ${imc}. (${nivelImc})`;
+
+    setResultado(msg, true);
 })
 
-function setResultado(msg){
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = `<p>${msg}</p>`;
+function getImc(peso, altura){
+    const imc = peso / (altura ** 2);
+    return imc.toFixed(2);
 }
+
+function getNivel(imc){
+    const nivel = ['Abaixo do peso', 'Peso Ideal', 'Sobrepeso', 
+        'obesidade grau1', 'obesidade grau 2', 'obesidade grau 3']
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function criaP (){
+    const p = document.createElement ('p');
+    return p;
+}
+
+function setResultado(msg, isValid){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    
+    const p = criaP();
+    if(isValid) {
+        p.classList.add('resultado')
+    } else {
+        p.classList.add('bad')
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+
+}  
